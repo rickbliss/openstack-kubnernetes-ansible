ac98eaea62467694ae9633995902fbb6c82158f3
rickbliss <rick.blss@gmail.com>
2017-11-23 07:56:38 -0500

    script to read project vars into tf vars

--- openstack-terraform/set-tf-os-vars.sh
+++ openstack-terraform/set-tf-os-vars.sh
@@ -1,6 +1,12 @@
-TF_VAR_openstack_user_name=$OS_USERNAME
-TF_VAR_openstack_tenant_name=$OS_TENANT_NAME
-TF_VAR_openstack_tenant_id=$OS_TENANT_ID
-TF_VAR_openstack_password=$openstack_password
-TF_VAR_openstack_auth_url=$OS_AUTH_URL
-
+#!/bin/bash
+source *openrc.sh*
+if [ -z "$OS_USERNAME" ]; then
+  echo "OpenStack Project RC must be sourced!"
+  exit 1
+fi
+export TF_VAR_openstack_user_name=$OS_USERNAME
+export TF_VAR_openstack_tenant_name=$OS_TENANT_NAME
+export TF_VAR_openstack_tenant_id=$OS_TENANT_ID
+export TF_VAR_openstack_password=$OS_PASSWORD
+export TF_VAR_openstack_auth_url=$OS_AUTH_URL
+echo "Terraform Varables set."
